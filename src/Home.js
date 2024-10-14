3725766736cea24c93e133dec8d53e5c56d164eb
Aliaa osama <aliaaosama97@gmail.com>
2024-10-14 14:35:29 +0200

    chapter 20 done

--- src/Home.js
+++ src/Home.js
@@ -1,16 +1,22 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import BlogList from "./BlogList";
+import useFetch from "./usefetch";
 
 const Home = () => {
-  const [blogs, setBlogs] = useState([
-    { title: 'My new website', body: 'lorem ipsum...', author: 'mario', id: 1 },
-    { title: 'Welcome party!', body: 'lorem ipsum...', author: 'yoshi', id: 2 },
-    { title: 'Web dev top tips', body: 'lorem ipsum...', author: 'mario', id: 3 }
-  ])
 
+  const {data: blogs, isPending , error} = useFetch('http://localhost:8000/blogs');
+
+  // const handleDelete = (id) => {
+  //   const newBlogs = blogs.filter(blog => blog.id !== id);
+  //   setBlogs(newBlogs);
+  // }
+
+ 
   return ( 
     <div className="home">
-       <BlogList blogs={blogs} title="All Blogs" />
+      { error && <div>{ error }</div> }
+      {isPending && <div>Loding...</div>}
+      {blogs && <BlogList blogs={blogs} />}
   </div>
   );
 }
